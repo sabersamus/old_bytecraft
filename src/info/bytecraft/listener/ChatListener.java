81766530080200a97e11afe61f1fe14c982ece45
Sabersamus <rcatron10@gmail.com>
2014-02-05 13:52:14 -0800

    Muted players cant talk,
    also fixes chat name color

--- src/info/bytecraft/listener/ChatListener.java
+++ src/info/bytecraft/listener/ChatListener.java
@@ -1,9 +1,14 @@
 package info.bytecraft.listener;
 
+import java.util.Date;
+
 import info.bytecraft.Bytecraft;
 import info.bytecraft.api.BytecraftPlayer;
+import info.bytecraft.api.PlayerReport;
+import info.bytecraft.api.BytecraftPlayer.ChatState;
 import info.bytecraft.api.Rank;
 import info.bytecraft.api.BytecraftPlayer.Flag;
+import info.bytecraft.api.PlayerReport.Action;
 import info.bytecraft.database.*;
 
 import org.bukkit.Bukkit;
@@ -28,10 +33,33 @@ public class ChatListener implements Listener
         String message = event.getMessage();
         
         BytecraftPlayer player = plugin.getPlayer(event.getPlayer());
+        if(player.getChatState() != ChatState.CHAT)return;
         if(player.hasFlag(Flag.MUTE)){
-            player.sendMessage(ChatColor.RED + "You are currently muted.");
-            event.setCancelled(true);
-            return;
+            try(IContext ctx = plugin.createContext()){
+                IReportDAO dao = ctx.getReportDAO();
+                for(PlayerReport report: dao.getReports(player)){
+                    if(report.getAction() != Action.MUTE){
+                        continue;
+                    }
+                    Date valid = report.getValidUntil();
+                    if(valid == null){
+                        continue;
+                    }
+                    
+                    if(valid.getTime() < System.currentTimeMillis()){
+                        player.setFlag(Flag.MUTE, false);
+                        player.sendMessage(ChatColor.AQUA + "You are no longer muted");
+                        event.setCancelled(true);
+                        return;
+                    }else{
+                        player.sendMessage(ChatColor.RED + "You are currently muted.");
+                        event.setCancelled(true);
+                        return;
+                    }
+                }
+            }catch(DAOException e){
+                throw new RuntimeException(e);
+            }
         }
         BytecraftPlayer to;
         ChatColor color = ChatColor.WHITE;
@@ -43,12 +71,20 @@ public class ChatListener implements Listener
                 color = ChatColor.WHITE;
             }
             
-            String coloredMessage = "<" + player.getDisplayName() + ChatColor.WHITE + "> " + color + message;
-            if(player.hasFlag(Flag.LORD)){
-                if(player.getRank() == Rank.SETTLER || player.getRank() == Rank.MEMBER){
-                    if(!player.hasFlag(Flag.SOFTWARNED) && !player.hasFlag(Flag.HARDWARNED)){
-                        coloredMessage = "<" + ChatColor.GREEN + "[Lord]" + player.getDisplayName() + "> " + color + message;
-                    }
+            ChatColor nameColor = player.getRank().getColor();
+            if((player.hasFlag(Flag.LORD) || player.hasFlag(Flag.NOBLE))
+                    && (player.getRank() == Rank.SETTLER || player.getRank() == Rank.MEMBER)){
+                nameColor = ChatColor.GOLD;
+            }else if(player.hasFlag(Flag.HARDWARNED) || player.hasFlag(Flag.SOFTWARNED)){
+                nameColor = ChatColor.GRAY;
+            }
+            
+            String coloredMessage = "<" + nameColor + player.getName() + ChatColor.WHITE + "> " + color + message;
+            
+            if (player.getRank() == Rank.SETTLER || player.getRank() == Rank.MEMBER) {
+                    if (player.hasFlag(Flag.LORD)) {
+                        coloredMessage =  "<" + ChatColor.GREEN + "[Lord]" +
+                                nameColor + player.getName() + "> " + color + message;
                 }
             }
             
