58514e7bed027ef604cea71669b33f03cbcfe26d
Sabersamus <rcatron10@gmail.com>
2014-02-12 00:00:01 -0800

    Update Schema changes

--- schema_changes.sql
+++ schema_changes.sql
@@ -1,3 +1,11 @@
 ALTER TABLE `zone` 
 ADD `zone_creative` ENUM( 'true', 'false' ) NOT NULL DEFAULT 'false' 
 AFTER `zone_hostile` 
+
+
+CREATE TABLE IF NOT EXISTS `player_password`(
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
