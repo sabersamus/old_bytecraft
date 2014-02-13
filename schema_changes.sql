774fb2d433e0e3b226835b721e6264b6e9e39506
Sabersamus <rcatron10@gmail.com>
2014-02-12 20:12:12 -0800

    Added badge table

--- schema_changes.sql
+++ schema_changes.sql
@@ -9,3 +9,10 @@ CREATE TABLE IF NOT EXISTS `player_password`(
   `password` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS `player_badge` (
+  `player_name` varchar(32) NOT NULL,
+  `badge` varchar(32) NOT NULL,
+  `badge_level` int(10) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`player_name`, `badge`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
