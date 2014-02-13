1986216bd6f42dbeb8832b201b9b13cdb8e4b677
Sabersamus <rcatron10@gmail.com>
2014-02-12 20:12:28 -0800

    Badge is now the enum value

--- src/info/bytecraft/api/Badge.java
+++ src/info/bytecraft/api/Badge.java
@@ -1,72 +1,40 @@
 package info.bytecraft.api;
 
-public class Badge
-{
-    
-    public enum BadgeType
+public enum Badge {
+
+    VETERAN("Veteran");
+
+    private final int maxLevel;
+    private final String name;
+
+    private Badge(String name)
     {
-        
-        VETERAN("Veteran");
-        
-        private final int maxLevel;
-        private final String name;
-        
-        private BadgeType(String name)
-        {
-            this(name, 1);
-        }
-        
-        private BadgeType(String name, int maxLevel)
-        {
-            this.maxLevel = maxLevel;
-            this.name = name;
-        }
+        this(name, 1);
+    }
 
-        public int getMaxLevel()
-        {
-            return maxLevel;
-        }
+    private Badge(String name, int maxLevel)
+    {
+        this.maxLevel = maxLevel;
+        this.name = name;
+    }
 
-        public String getName()
-        {
-            return name;
-        }
+    public int getMaxLevel()
+    {
+        return maxLevel;
+    }
+
+    public String getName()
+    {
+        return name;
     }
     
-    private BadgeType type;
-    private int level;
-    
-    public Badge(BadgeType type, int level)
+    public static Badge fromString(String name)
     {
-        if(level > type.getMaxLevel()){
-            level = type.getMaxLevel();
+        for(Badge badge: values()){
+            if(badge.name.equalsIgnoreCase(name)){
+                return badge;
+            }
         }
-        
-        this.setLevel(level);
-        this.setType(type);
+        return null;
     }
-
-    public BadgeType getType()
-    {
-        return type;
-    }
-
-    public void setType(BadgeType type)
-    {
-        this.type = type;
-    }
-
-    public int getLevel()
-    {
-        return level;
-    }
-
-    public void setLevel(int level)
-    {
-        if(level > type.getMaxLevel()){
-            level = type.getMaxLevel();
-        }
-        this.level = level;
-    }
-    
 }
